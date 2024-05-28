18aca3bda27fc3557fe3e697d4569b92b074f263
Vinicius Silva Almeida <111448740+Vinicius-2003@users.noreply.github.com>
2024-05-28 12:23:53 -0300

    update

--- Faculdade/Funções/ex06/script.js
+++ Faculdade/Funções/ex06/script.js
@@ -1,8 +1,7 @@
-let horas = new Date().getHours();
-document.write(horas)
-let img = document.getElementById('imagem')
-
-
-if(horas <= 12){
-    img.src= "./midia/images.jpeg"
+var hora = new Date().getHours();
+let agora = 14;
+if (hora < 12){
+    document.write('<img src="midia/images.jpeg"/>')
+} else {
+    document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqx7yQC53oQNXk8Q5e0Vmt5xz9Dryecur70w&usqp=CAU"/>')
 }
